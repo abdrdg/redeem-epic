94a462382aab29a282fdc0ac89ac5ed6d1e30b35
Allan Bernard De Guzman <abeydeguzman@gmail.com>
2020-03-17 02:19:32 +0800

    Fixed layout

--- accounts_window.py
+++ accounts_window.py
@@ -16,14 +16,15 @@ password_input = Entry (root, show="*", textvariable = password)
 submit_button = Button(root, text="Save!")
 
 # Account entries:
-Label(root, text="Email: ").grid(column = 0, row = 1)
-email_input.grid(column = 1, row = 1)
-Label(root, text="Password: ").grid(column = 2, row = 1)
-password_input.grid(column = 3, row = 1)
-submit_button.grid(column = 4, row = 1)
+Label(root, text="ACCOUNTS").grid(column = 0, row = 0)
+Label(root, text="Email: ").grid(column = 1, row = 0)
+email_input.grid(column = 2, row = 0)
+Label(root, text="Password: ").grid(column = 3, row = 0)
+password_input.grid(column = 4, row = 0)
+submit_button.grid(column = 5, row = 0)
 
 count = 0
 for index in range(int(conn.user_len)):
-    Label(root, text = "Account "+str((index+1))+": "+ conn.get_user(index)).grid(column = 0, row = index+4)
+    Label(root, text = str((index+1))+"     "+ conn.get_user(index)).grid(column = 0, row = index+4)
 
 root.mainloop()
