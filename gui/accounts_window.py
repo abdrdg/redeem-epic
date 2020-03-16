57d80f668497e294d05d683cc52a451f4f3a314a
Allan Bernard De Guzman <abeydeguzman@gmail.com>
2020-03-17 01:42:35 +0800

    Added basic layout without functionality

--- gui/accounts_window.py
+++ gui/accounts_window.py
@@ -1,8 +1,30 @@
 from tkinter import *
 
 root = Tk()
+root.title("Epic Games Account")
 
-text = Label(root, text="Hello World")
-text.pack()
+# String vars
+username = StringVar()
+password = StringVar()
+
+# Initializing
+email_input = Entry(root, textvariable = username)
+password_input = Entry (root, show="*", textvariable = password)
+submit_button = Button(root, text="Save!")
+
+
+# Account entries:
+Label(root, text="Email: ").grid(column = 0, row = 1)
+email_input.grid(column = 1, row = 1)
+Label(root, text="Password: ").grid(column = 2, row = 1)
+password_input.grid(column = 3, row = 1)
+submit_button.grid(column = 4, row = 1)
+
+count = 0
+for items in range(10):
+    Label(root, text = "Account "+str((items+1))+": ").grid(column = 0, row = items+4)
+
+
+
 
 root.mainloop()
